29c163b8227e70d8863da60eedc28dad74afb411
Nicolas Reis <nicolas.reeis2024@gmail.com>
2025-08-15 19:14:01 -0300

    Update TabuadaEmLoop.java
    Este programa em Java solicita ao usuário que digite um número e, em seguida, exibe a sua tabuada de 1 a 10. O processo se repete até que o usuário insira -1, momento em que o programa é encerrado. Foi utilizado Scanner para entrada de dados e um laço for para gerar a tabuada.

--- TabuadaEmLoop.java
+++ TabuadaEmLoop.java
@@ -1,1 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Digite um numero: [insira -1 para encerrar] ");
+            int numero = sc.nextInt();
+
+            if(numero == -1){
+                System.out.println("Programa encerrado!");
+                break;
+            }
+            else{
+                for(int i=1; i < 11;i++){
+                    System.out.printf("%d x %d = %d\n", numero, i, numero*i);
+                }
+            }
+        }
+        sc.close();
+    }
+}
 
