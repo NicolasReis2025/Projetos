b75226a81a0343a219b76291940618d9b86ec924
Nicolas Reis <nicolas.reeis2024@gmail.com>
2025-08-15 15:54:51 -0300

    Update ClassificadorDados.java
       Neste código, o programa solicita ao usuário o nome, a idade, o peso e a altura de cada pessoa.
        Após o cadastro, ele identifica e exibe a pessoa mais velha e a mais nova, utilizando métodos como indexOf() e get(), partindo do índice zero como referência inicial.
    Para a manipulação e classificação de dados, como pesos, é utilizada a classe Collections da biblioteca padrão do Java.
         O intuito deste código é mostrar que a mesma funcionalidade pode ser implementada de várias formas, ficando a critério do programador escolher a abordagem mais eficiente para cada caso.

--- ClassificadorDados.java
+++ ClassificadorDados.java
@@ -1,1 +1,84 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> nomes = new ArrayList<>();
+        ArrayList<Integer> idades = new ArrayList<>();
+        ArrayList<Double> pesos = new ArrayList<>();
+
+        int totalIdades = 0;
+        double totalPeso = 0.0;
+
+        System.out.println("Digite o numero de pessoas cadastradas: ");
+        int quantidadepessoas = sc.nextInt();
+
+        for(int i=0; i < quantidadepessoas; i++){
+            System.out.println((i+1) + "# pessoa");
+            System.out.println("Nome: ");
+            sc.nextLine();
+            String nome = sc.nextLine();
+            System.out.println("idade: ");
+            int idade = sc.nextInt();
+            totalIdades += idade;
+
+            System.out.println("peso: ");
+            double peso = sc.nextDouble();
+            
+            nomes.add(nome);
+            idades.add(idade);
+            pesos.add(peso);
+        }
+
+        int maiorIdade = idades.get(0);
+        int posMaiorIdade = 0;
+
+        int menorIdade = idades.get(0);
+        int posMenorIdade = 0;
+
+        double mediaIdade = (double) totalIdades / idades.size();
+
+        System.out.println("Pessoa mais velha: ");
+        for(int i=0; i < idades.size(); i++){
+            if(idades.get(i) > maiorIdade){
+                maiorIdade = idades.get(i);
+                posMaiorIdade = i;
+            }
+
+            if(idades.get(i) < menorIdade){
+                menorIdade = idades.get(i);
+                posMenorIdade = i;
+            }
+        }
+        System.out.println("==== RESULTADOS ====");
+        System.out.printf("media do grupo de idade: %.2f\n", mediaIdade);
+        System.out.println("Pessoa mais velha: ");
+        System.out.printf("Nome: %s\nIdade: %d anos de idade\n", nomes.get(posMaiorIdade), maiorIdade);
+        System.out.println();
+        System.out.println("Pessoa mais nova: ");
+        System.out.printf("Nome: %s\nIdade: %d anos de idade\n", nomes.get(posMenorIdade), menorIdade);
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+        double maiorPeso = Collections.max(pesos);
+        int posMaiorPeso = pesos.indexOf(maiorPeso);
+
+        double menorPeso = Collections.min(pesos);
+        int posMenorPeso = pesos.indexOf(menorPeso);
+
+        double mediaPeso = totalPeso / pesos.size();
+
+        System.out.println("Pessoa com o maior peso: ");
+        System.out.printf("Nome: %s\nPeso: %.2fkg\n", nomes.get(posMaiorPeso), maiorPeso);
+        System.out.println();
+        System.out.println("Pessoa com o menor peso: ");
+        System.out.printf("Nome: %s\nPeso: %.2fKg\n", nomes.get(posMenorPeso), menorPeso);
+
+
+        sc.close();
+    }
+}
 
